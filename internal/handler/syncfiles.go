dde4ec87e5e340f8aaaa11aa3c7bcb30fa6842ae
jonntd <jonntd@gmail.com>
2025-05-26 23:21:49 +0800

    refactor(syncfiles): 优化rclone同步命令参数
    移除冗余参数并调整同步策略，主要变更包括：
    - 仅同步目录结构，添加--dirs-only参数
    - 统一使用--encoding参数替代--115-encoding
    - 删除不必要的文件过滤参数
    - 格式化命令参数提高可读性

--- internal/handler/syncfiles.go
+++ internal/handler/syncfiles.go
@@ -127,7 +127,17 @@ func runRcloneSync(sourceDir, remoteDest string, colonIndex int) error {
 	fmt.Println("sourceDirt:", sourceDir)
 	fmt.Println("remoteDest:", remoteDest)
 
-	cmd := exec.Command("rclone", "sync", sourceDir, filepath.Join(remoteDest, sourceDir[colonIndex+1:]), "--fast-list", "--checkers", "4", "--tpslimit", "5", "--log-level", "INFO", "--delete-after", "--size-only", "--ignore-times", "--ignore-existing", "--ignore-checksum", "--max-size", "10M", "--transfers", "4", "--multi-thread-streams", "0", "--local-encoding", "Slash,InvalidUtf8", "--115-encoding", "Slash,InvalidUtf8", "--exclude", "*.strm")
+	// cmd := exec.Command("rclone", "sync", sourceDir, filepath.Join(remoteDest, sourceDir[colonIndex+1:]), "--fast-list", "--checkers", "4", "--tpslimit", "5", "--log-level", "INFO", "--delete-after", "--size-only", "--ignore-times", "--ignore-existing", "--ignore-checksum", "--max-size", "10M", "--transfers", "2", "--multi-thread-streams", "0", "--local-encoding", "Slash,InvalidUtf8", "--115-encoding", "Slash,InvalidUtf8", "--exclude", "*.strm")
+	cmd := exec.Command("rclone", "sync", sourceDir, filepath.Join(remoteDest, sourceDir[colonIndex+1:]),
+		"--fast-list",
+		"--checkers", "4",
+		"--tpslimit", "5",
+		"--log-level", "INFO",
+		"--delete-after", // 如果需要删除目标中多余的目录
+		"--dirs-only",    // 核心：只同步目录
+		"--local-encoding", "Slash,InvalidUtf8",
+		"--encoding", "Slash,InvalidUtf8")
+
 	fmt.Printf("Running command: %s\n", cmd.String())
 
 	stdout, err := cmd.StdoutPipe()
@@ -192,7 +202,7 @@ func runRcloneSync(sourceDir, remoteDest string, colonIndex int) error {
 
 func createStrmFiles(sourceDir, remoteDest string, colonIndex int) error {
 	// 使用filepath.Join和config.RootDir获取rclone的绝对路径
-	cmd := exec.Command("rclone", "lsf", "-R", sourceDir, "-vv", "--files-only", "--min-size", "100M", "--checkers", "4", "--transfers", "4", "--tpslimit", "5", "--multi-thread-streams", "0", "--local-encoding", "Slash,InvalidUtf8", "--115-encoding", "Slash,InvalidUtf8")
+	cmd := exec.Command("rclone", "lsf", "-R", sourceDir, "-vv", "--files-only", "--min-size", "100M", "--checkers", "4", "--transfers", "4", "--tpslimit", "5", "--multi-thread-streams", "0", "--local-encoding", "Slash,InvalidUtf8", "--encoding", "Slash,InvalidUtf8")
 	fmt.Printf("Running command: %s\n", cmd.String())
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
